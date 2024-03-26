bf0b6a0973ed52c2c4d863a913c9c160da72a69f
SumayiaOishi <sumayia.alam04@gmail.com>
2024-03-26 22:12:03 +0600

    components

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Card from "./components/card";
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/card.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Cat from "./cat.jpg";
+
+export default function Card() {
+  return (
+    
+      <div className="card " style={{ width: "18rem" }}>
+        <img src={Cat} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="/" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    
+  );
+}
