e77c99df8b8770f8972f996a9ebbdb5853dd2db1
SumayiaOishi <sumayia.alam04@gmail.com>
2024-04-01 01:51:08 +0600

    doing button for todo list

--- src/components/Todo_List.tsx
+++ src/components/Todo_List.tsx
@@ -3,19 +3,25 @@ import React, { useState } from "react";
 
 
 export default function Todo_List() {
-  const [text, setText]=useState('');
+  const [text, setText]=useState("");
   const[list, setList]=useState([]);
  
   const getInput=(event: { target: { value: React.SetStateAction<string>; }; })=>{
        setText(event.target.value);
   }
-  const added=()=>{
-     
+  const AddToList=()=>{
+    list.push(text);
+    setList([...list]);
   }
   return (
     <div className="main1">
+     
        <input type="text" placeholder="Add New Task" className="input1"  onChange={getInput}/>
-       <button type="submit" className="btn btn-success" onClick={added}>Add</button>
+       <button type="submit" className="btn btn-success" onClick={AddToList}>Add</button>
+
+       <ul>
+        
+       </ul>
     </div>
   )
 }
